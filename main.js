f755b206fb4d618061c76d417133cf8162de852b
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2021-09-29 21:06:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,6 @@ function MineSweeper(){
   
   let _2Dgrid = []
  
-  function buildGrid() {
-    for (let row = 0; row < rows; row++){ _2Dgrid.push([]);
-      for (let col = 0; col < cols; col++) {
-        _2Dgrid.push(empty)
-      }
-    }
-  }
   
   function layMines() {
   
