1ca8f56e661c7e593bf35741f8dc210580651ebd
Neur0plasticity <41029495+Neur0plasticity@users.noreply.github.com>
2021-09-29 21:03:29 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+function MineSweeper(){
+ 
+  const encodings = ["empty","mine","flag"];
+  
+  let _2Dgrid = []
+ 
+  function buildGrid() {
+    for (let row = 0; row < rows; row++){ _2Dgrid.push([]);
+      for (let col = 0; col < cols; col++) {
+        _2Dgrid.push(empty)
+      }
+    }
+  }
+  
+  function layMines() {
+  
+  }
+  
+  function layHints() {
+  
+  }
+  
+  function flag(){
+  
+  }
+  function press(){
+  
+  } 
+  
+  
+}
